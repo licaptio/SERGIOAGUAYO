c6870c834def9850f3fa40ffbb5b772ed4dcf037
licaptio <licaptio42@gmail.com>
2026-02-03 17:10:54 -0600

    Update services.js

--- BDSERGIOAGUAYO25/services.js
+++ BDSERGIOAGUAYO25/services.js
@@ -1,7 +1,7 @@
 // js/services.js
 
 /* =========================
-   🔥 FIREBASE AUTH
+   🔥 FIREBASE AUTH (COMPAT)
 ========================= */
 import "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
 import "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js";
@@ -10,14 +10,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
   authDomain: "inventariopv-643f1.firebaseapp.com",
   projectId: "inventariopv-643f1",
-  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  storageBucket: "inventariopv-643f1.appspot.com",
   messagingSenderId: "96242533231",
   appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// ✅ INICIALIZACIÓN CORRECTA
+firebase.initializeApp(firebaseConfig);
 
+// ✅ EXPORTAS AUTH
 export const auth = firebase.auth();
 
 /* =========================
